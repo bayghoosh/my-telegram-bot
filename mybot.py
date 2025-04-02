98a2939ad4cbeb11fa3ac6222f8cf1d7de8e13f0
bayghoosh <tebe.atfal@gmail.com>
2025-04-02 11:58:49 +0330

    mybot.py

--- mybot.py
+++ mybot.py
@@ -1,15 +1,15 @@
 from telegram import Update
-     from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-     def start(update: Update, context: CallbackContext):
-         update.message.reply_text('ÓáÇã! Èå ÑÈÇÊ ãä ÎæÔ ÇæãÏí! ??')
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Ø³Ù„Ø§Ù…! Ø¨Ù‡ Ø±Ø¨Ø§Øª Ù…Ù† Ø®ÙˆØ´ Ø§ÙˆÙ…Ø¯ÛŒ! ðŸ˜Š')
 
-     def main():
-         updater = Updater("AAFdnqvhJu-AhgRu0oAEYpSwv0N9UfwwfKE", use_context=True)
-         dispatcher = updater.dispatcher
-         dispatcher.add_handler(CommandHandler("start", start))
-         updater.start_polling()
-         updater.idle()
+def main():
+    updater = Updater("AAFdnqvhJu-AhgRu0oAEYpSwv0N9UfwwfKE", use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
 
-     if name == 'main':
-         main()
+if name == 'main':
+    main()
